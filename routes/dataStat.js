be9ba5cb24568bb1d71156bb53acf96f64004f94
MD-Sayem <mdsayemislam56@gmail.com>
2022-12-05 00:29:24 +0600

    authentication for veiwing stastistics

--- routes/dataStat.js
+++ routes/dataStat.js
@@ -5,28 +5,35 @@ const User=require('../models/user');
 
 router.get('/data-stat',function(req,res){
 
-  let user=req.user.username;
-  Data.aggregate(
-  [
-    {
-     "$group" : {
-        _id : "$status",
-        count: { $sum: 1 }
-      }
-    },
-    {
-      $sort : { _id: 1 }
-    }
-  ]
-  ).exec((err, results) => {
-      if (err) throw err;
+  if(req.isAuthenticated() && req.user.role=='admin'){
 
-      res.render('dataStat',{stat:results});
-    //  res.send(results);
-      // let size=Object.objsize(results);
-    //  console.log(results.length);
-    //  console.log(results[0]._id);
-  })
+        let user=req.user.username;
+        Data.aggregate(
+        [
+          {
+           "$group" : {
+              _id : "$status",
+              count: { $sum: 1 }
+            }
+          },
+          {
+            $sort : { _id: 1 }
+          }
+        ]
+        ).exec((err, results) => {
+            if (err) throw err;
+
+            res.render('dataStat',{stat:results});
+          //  res.send(results);
+            // let size=Object.objsize(results);
+          //  console.log(results.length);
+          //  console.log(results[0]._id);
+        });
+     }  
+     else{
+         res.redirect('/admin-login');
+       }
+
 
 });
 
