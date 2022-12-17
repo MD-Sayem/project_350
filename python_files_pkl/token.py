325d0a353a24a4d8ff0f40a2284d35dfccbcb9bf
Mahir Mosleh <mahirmosleh136@gmail.com>
2022-12-17 15:50:00 +0200

    added the pkl files for the models

---
+++ python_files_pkl/token.py
@@ -0,0 +1,18 @@
+import unicodedata
+#print('hello')
+def tokenize(text):
+    bn_nums = set([chr(i) for i in range(2432, 2560) if unicodedata.category(chr(i)).startswith("N")])
+    bn_letters = set([chr(i) for i in range(2432, 2560) if
+                      not unicodedata.category(chr(i)).startswith("C") and not unicodedata.category(chr(i)).startswith(
+                          "N")])
+    bn_letters_nums = bn_nums | bn_letters
+    all_letters_nums = bn_letters_nums 
+    if not text:
+        return []
+    new_text = ''
+    for c in text:
+        new_text += c if c in all_letters_nums else ' '
+    return new_text.split()
+
+#print(tokenize("তাঁরা মা–বাবার কান্না থামছে না।"))
+# sys.stdout.flush()
